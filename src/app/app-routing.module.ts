af9fcdfc61556c22e5a719eda57745a9bb2ad716
KraenauDiegoDoApps <diego.kraeneau@doapps.me>
2021-06-18 19:24:09 -0500

    Task01

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GastosComponent } from './components/gastos/gastos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'gastos', component: GastosComponent },
+  { path: '', component: GastosComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
